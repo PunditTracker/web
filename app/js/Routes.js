3288ebe9686db818c8445c9da426abb577a12535
Jake Marsh <jakemmarsh@gmail.com>
2014-11-12 13:17:52 -0500

    Add pages for login, register, and general categories

--- app/js/Routes.js
+++ app/js/Routes.js
@@ -9,7 +9,10 @@ var DefaultRoute   = require('react-router').DefaultRoute;
 var NotFoundRoute  = require('react-router').NotFoundRoute;
 var App            = require('./App');
 var HomePage       = require('./pages/HomePage');
+var LoginPage      = require('./pages/LoginPage');
+var RegisterPage   = require('./pages/RegisterPage');
 var SearchPage     = require('./pages/SearchPage');
+var CategoryPage   = require('./pages/CategoryPage');
 var PredictionPage = require('./pages/PredictionPage');
 var UserPage       = require('./pages/UserPage');
 var NotFoundPage   = require('./pages/NotFoundPage');
@@ -19,10 +22,18 @@ module.exports = (
 
     <Route handler={App}>
       <DefaultRoute handler={HomePage} />
+
       <Route name='Home' path='/' handler={HomePage} />
+
+      <Route name='Login' path='/login' handler={LoginPage} />
+      <Route name='Register' path='/register' handler={RegisterPage} />
+
       <Route name='Search' path='/search' handler={SearchPage} />
+
+      <Route name='Category' path='/category/:category' handler={CategoryPage} />
       <Route name='Prediction' path='/prediction/:id' handler={PredictionPage} />
       <Route name='Profile' path='/user/:id' handler={UserPage} />
+
       <NotFoundRoute handler={NotFoundPage} />
     </Route>
 
---
+++ app/js/pages/LoginPage.js
@@ -0,0 +1,18 @@
+/**
+ * @jsx React.DOM
+ */
+'use strict';
+
+var React = require('react/addons');
+
+var LoginPage = React.createClass({
+
+  render: function() {
+    return (
+      <div />
+    );
+  }
+
+});
+
+module.exports = React.createFactory(LoginPage);
