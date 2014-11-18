56911154a6256bc7998f9cd1575dd63b1df09a73
Jake Marsh <jakemmarsh@gmail.com>
2014-11-18 16:52:21 -0500

    add own node.js server for deployment to Heroku

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -3,7 +3,7 @@
 var gulp   = require('gulp');
 var config = require('../config');
 
-gulp.task('watch', ['browserSync', 'server'], function() {
+gulp.task('watch', function() {
   // Scripts are automatically watched by Watchify inside Browserify task
   gulp.watch(config.styles.src,                 ['sass']);
   gulp.watch(config.images.src,                 ['imagemin', 'reload']);
