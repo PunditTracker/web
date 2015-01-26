fe38ef5dd6d35eff3782452d95975a98d9b7badf
Jake Marsh <jakemmarsh@gmail.com>
2015-01-26 18:37:56 -0500

    finish logic for automation cdnization and deployment to S3

---
+++ config.js
@@ -0,0 +1,9 @@
+var config = {};
+
+config.aws = {
+  bucket: 'assets.pundittracker.com',
+  key: 'AKIAJTL7QEGHS6OTMSMQ',
+  secret: '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'
+};
+
+module.exports = config;
--- gulp/tasks/deploy.js
+++ gulp/tasks/deploy.js
@@ -1,10 +1,30 @@
 'use strict';
 
-var gulp   = require('gulp');
-//var config = require('../config');
+var gulp         = require('gulp');
+var rename       = require('gulp-rename');
+var awspublish   = require('gulp-awspublish');
+var config       = require('../config');
+var globalConfig = require('../../config');
 
-gulp.task('deploy', function() {
+gulp.task('deploy', ['cdnizer'], function() {
 
-  // Deploy to hosting environment
+  var publisher = awspublish.create({
+    key: globalConfig.aws.key,
+    secret: globalConfig.aws.secret,
+    bucket: globalConfig.aws.bucket
+  });
+  var oneWeekInSeconds = 60*60*24*7;
+  var headers = {
+    'Cache-Control': 'max-age=' + oneWeekInSeconds + ', no-transform, public'
+  };
+
+  // Assets
+  return gulp.src(config.buildDir + '**/*.{json,js,css,eot,svg,ttf,woff,otf,png,jpg,jpeg}')
+  .pipe(rename(function(path) {
+    path.dirname = 'app/' + path.dirname;
+  }))
+  .pipe(awspublish.gzip())
+  .pipe(publisher.publish(headers))
+  .pipe(awspublish.reporter());
 
 });
