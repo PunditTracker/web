9438098a4e68a0bd292470a65d6f51bf4a1053a9
Jake Marsh <jakemmarsh@gmail.com>
2015-02-10 13:11:54 -0500

    fix predict page styling and logic

--- app/js/pages/PredictPage.js
+++ app/js/pages/PredictPage.js
@@ -12,6 +12,7 @@ var APIUtils                = require('../utils/APIUtils');
 var PredictionAPI           = require('../utils/PredictionAPI');
 var AuthenticatedRouteMixin = require('../mixins/AuthenticatedRouteMixin');
 var DocumentTitle           = require('../components/DocumentTitle');
+var PredictionCard          = require('../components/PredictionCard');
 var User                    = require('../components/User');
 var TagInput                = require('../components/TagInput');
 
@@ -43,11 +44,11 @@ var PredictPage = React.createClass({
       clause: null,
       joiner: null,
       category: null,
-      subcategory: null,
       tags: [],
       doesExpire: false,
       deadline: null,
       submitDisabled: true,
+      createdPrediction: null,
       error: null
     };
   },
@@ -78,14 +79,6 @@ var PredictPage = React.createClass({
     this.setState({ category: hasCategory ? evt.target.value : null });
   },
 
-  setSubcategory: function(evt) {
-    var hasSubcategory = evt.target.value.indexOf('...') === -1;
-
-    evt.preventDefault();
-
-    this.setState({ subcategory: hasSubcategory ? evt.target.value : null });
-  },
-
   addTag: function(tag) {
     var tagsCopy = this.state.tags;
 
@@ -113,10 +106,6 @@ var PredictPage = React.createClass({
 
     evt.preventDefault();
 
-    if ( this.state.subcategory ) {
-      prediction.tags.push(this.state.subcategory);
-    }
-
     if ( this.state.tags && this.state.tags.length ) {
       prediction.tags = prediction.tags.concat(this.state.tags);
     }
@@ -134,10 +123,8 @@ var PredictPage = React.createClass({
       prediction.title += '.';
     }
 
-    console.log('post to API:', prediction);
-
-    PredictionAPI.postPrediction(prediction).then(function() {
-      this.setState({ posted: true, error: null });
+    PredictionAPI.postPrediction(prediction).then(function(createdPrediction) {
+      this.setState({ posted: true, createdPrediction: createdPrediction, error: null });
     }.bind(this)).catch(function(err) {
       this.setState({ error: err });
     }.bind(this));
@@ -155,37 +142,6 @@ var PredictPage = React.createClass({
     }
 
     return elements;
-  },
-
-  renderSubcategoryDropdown: function() {
-    var element = null;
-    var options = _.map(['Select a Subcategory...', 'NFL', 'NBA', 'MLB', 'NHL'], function(subcategory, index) {
-      return (
-        <option key={index}>{subcategory}</option>
-      );
-    });
-
-    if ( this.state.category && this.state.category.toLowerCase() === 'sports' ) {
-      element = (
-        <select name="category" onChange={this.setSubcategory}>
-          {options}
-        </select>
-      );
-    }
-
-    return element;
-  },
-
-  renderSubcategoryTag: function() {
-    var element = null;
-
-    if ( this.state.subcategory ) {
-      element = (
-        <li>{this.state.subcategory}</li>
-      );
-    }
-
-    return element;
   },
 
   renderTags: function() {
@@ -258,91 +214,90 @@ var PredictPage = React.createClass({
       'date-input': true,
       'hidden': !this.state.doesExpire
     });
+
+    return (
+      <form id="prediction-form" className="prediction-form" onSubmit={this.handleSubmit}>
+        <h4>I predict that...</h4>
+        <fieldset>
+          <input type="text"
+                 placeholder={this.constructor.placeholderOne}
+                 name="prediction"
+                 className="prediction-input"
+                 valueLink={this.linkState('prediction')} />
+          <ul className="joiners">
+            <span className="is-opt">(Optional)</span>
+            <li><button className={ifClasses} onClick={this.setJoiner.bind(null, 'if')}>if</button></li>
+            <li><button className={andClasses} onClick={this.setJoiner.bind(null, 'and')}>and</button></li>
+            <li><button className={unlessClasses} onClick={this.setJoiner.bind(null, 'unless')}>unless</button></li>
+            <li className={clauseWrapperClasses}>
+              <input type="text"
+                     placeholder={this.constructor.placeholderTwo}
+                     name="clause"
+                     className="clause-input"
+                     valueLink={this.linkState('clause')} />
+            </li>
+          </ul>
+        </fieldset>
+        <fieldset>
+          <select name="category" onChange={this.setCategory}>
+            <option>Select a Category...</option>
+            {this.renderCategoryOptions()}
+          </select>
+          </fieldset>
+          <fieldset>
+          <TagInput ref="tagInput" placeholder="Add tags (Optional)" addTag={this.addTag} />
+        </fieldset>
+        <fieldset>
+          <input type="radio" ref="doesntExpire" name="doesnt-expire" checked={!this.state.doesExpire} onChange={this.toggleDateInput.bind(null, false)} />
+          <label htmlFor="doesnt-expire">Doesn't expire</label>
+          <input type="radio" name="does-expire" id="date-exists-2" checked={this.state.doesExpire} onChange={this.toggleDateInput.bind(null, true)} />
+          <label htmlFor="does-expire">Expires</label>
+          <input name="date" type="date" className={dateInputClasses} onChange={this.setDeadline} />
+        </fieldset>
+        <input name="login" defaultValue="yes" hidden />
+        {this.renderError()}
+        <input type="submit" className="button" defaultValue="Publish" disabled={this.state.submitDisabled ? 'true' : ''} />
+      </form>
+    );
+  },
+
+  renderSuccessMessage: function() {
+    return (
+      <div>
+        <h4 className="text-center nudge-half--bottom flush--top">Prediction submitted!</h4>
+        <Link to="Home" className="btn block full-width text-center">Back to Home</Link>
+      </div>
+    );
+  },
+
+  renderPreviewCard: function() {
     var finalPredictionClasses = cx({
       'final-prediction': true,
       'placeholder': !this.state.prediction
     });
 
     return (
-      <div className="container card-grid">
-        <div className="content-with-sidebar left">
-          <form id="prediction-form" className="prediction-form" onSubmit={this.handleSubmit}>
-            <h4>I predict that...</h4>
-            <fieldset>
-              <input type="text"
-                     placeholder={this.constructor.placeholderOne}
-                     name="prediction"
-                     className="prediction-input"
-                     valueLink={this.linkState('prediction')} />
-              <ul className="joiners">
-                <span className="is-opt">(Optional)</span>
-                <li><button className={ifClasses} onClick={this.setJoiner.bind(null, 'if')}>if</button></li>
-                <li><button className={andClasses} onClick={this.setJoiner.bind(null, 'and')}>and</button></li>
-                <li><button className={unlessClasses} onClick={this.setJoiner.bind(null, 'unless')}>unless</button></li>
-                <li className={clauseWrapperClasses}>
-                  <input type="text"
-                         placeholder={this.constructor.placeholderTwo}
-                         name="clause"
-                         className="clause-input"
-                         valueLink={this.linkState('clause')} />
-                </li>
-              </ul>
-            </fieldset>
-            <fieldset>
-              <select name="category" onChange={this.setCategory}>
-                <option>Select a Category...</option>
-                {this.renderCategoryOptions()}
-              </select>
-              {this.renderSubcategoryDropdown()}
-              </fieldset>
-              <fieldset>
-              <TagInput ref="tagInput" placeholder="Add tags (Optional)" addTag={this.addTag} />
-            </fieldset>
-            <fieldset>
-              <input type="radio" ref="doesntExpire" name="doesnt-expire" checked={!this.state.doesExpire} onChange={this.toggleDateInput.bind(null, false)} />
-              <label htmlFor="doesnt-expire">Doesn't expire</label>
-              <input type="radio" name="does-expire" id="date-exists-2" checked={this.state.doesExpire} onChange={this.toggleDateInput.bind(null, true)} />
-              <label htmlFor="does-expire">Expires</label>
-              <input name="date" type="date" className={dateInputClasses} onChange={this.setDeadline} />
-            </fieldset>
-            <input name="login" defaultValue="yes" hidden />
-            {this.renderError()}
-            <input type="submit" className="button" defaultValue="Publish" disabled={this.state.submitDisabled ? 'true' : ''} />
-          </form>
+      <div className="prediction-card final-prediction hover">
+        <div className="background"><div className="scrim" /></div>
+        <div className="tags">
+          <ul className="inner">
+            <li className="category">{this.state.category || 'Select a Category...'}</li>
+            {this.renderTags()}
+          </ul>
         </div>
-
-        <div className="sidebar right">
-          <div className="pure-g card-grid">
-            <div className="pure-u-1">
-              <div className="prediction-card final-prediction hover">
-                <div className="background"><div className="scrim" /></div>
-                <div className="tags">
-                  <ul className="inner">
-                    <li className="category">{this.state.category || 'Select a Category...'}</li>
-                    {this.renderSubcategoryTag()}
-                    {this.renderTags()}
-                  </ul>
-                </div>
-                <h4 className="text">
-                  <span className={finalPredictionClasses}>{this.state.prediction || this.constructor.placeholderOne}</span>
-                  {this.renderFinalJoiner()}
-                  {this.renderFinalClause()}
-                </h4>
-                <User user={this.props.currentUser} />
-              </div>
-            </div>
-          </div>
-        </div>
+        <h4 className="text">
+          <span className={finalPredictionClasses}>{this.state.prediction || this.constructor.placeholderOne}</span>
+          {this.renderFinalJoiner()}
+          {this.renderFinalClause()}
+        </h4>
+        <User user={this.props.currentUser} />
       </div>
     );
   },
 
-  renderSuccessMessage: function() {
+  renderCreatedPrediction: function() {
     return (
-      <div className="container slim">
-        <h4 className="text-center nudge-half--bottom flush--top">Forget your password?</h4>
-        <Link to="Home" className="btn block full-width text-center">Back to Home</Link>
-      </div>
+      <PredictionCard prediction={this.state.createdPrediction} />
     );
   },
 
@@ -352,7 +307,19 @@ var PredictPage = React.createClass({
 
         <DocumentTitle title="Predict" />
 
-        {this.state.posted ? this.renderSuccessMessage() : this.renderForm()}
+        <div className="container card-grid">
+          <div className="content-with-sidebar left hard--bottom">
+            {this.state.posted ? this.renderSuccessMessage() : this.renderForm()}
+          </div>
+
+          <div className="sidebar relative right hard--bottom">
+            <div className="pure-g card-grid">
+              <div className="pure-u-1">
+                {this.state.posted ? this.renderCreatedPrediction() : this.renderPreviewCard()}
+              </div>
+            </div>
+          </div>
+        </div>
 
       </section>
     );
