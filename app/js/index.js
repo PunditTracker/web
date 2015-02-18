5af29b0c18b08ff4845f439c58277de8edbc6df0
Jake Marsh <jakemmarsh@gmail.com>
2015-02-18 18:40:27 -0500

    fix routing for s3 hosting

--- app/js/index.js
+++ app/js/index.js
@@ -9,6 +9,8 @@ if ( process.env.NODE_ENV !== 'production' ) {
   window.React = React; // Enable React devtools
 }
 
+Router.HistoryLocation.replace(Router.HashLocation.getCurrentPath());
+
 Router.run(routes, Router.HistoryLocation, function(Handler, state) {
   React.render(<Handler params={state.params} query={state.query} />, document.getElementById('app'));
 });
