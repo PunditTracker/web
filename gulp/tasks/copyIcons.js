21afdd9b9ca61070f528c7ee0f6b9dc8033e9df7
Jake Marsh <jakemmarsh@gmail.com>
2015-02-18 19:25:34 -0500

    fix copyicons task

--- gulp/tasks/copyIcons.js
+++ gulp/tasks/copyIcons.js
@@ -1,11 +1,12 @@
 'use strict';
 
 var gulp   = require('gulp');
+var config = require('../config');
 
 gulp.task('copyIcons', function() {
 
-  // Copy icons from root directory to build/
-  return gulp.src(['./*.{png,ico,xml,json}'])
+  // Copy icon files from source directory to build/
+  return gulp.src(config.sourceDir + '*.{png,ico,xml,json}')
     .pipe(gulp.dest('build/'));
 
 });
