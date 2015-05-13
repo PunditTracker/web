bd5f3e5e5d038c6242d169384ed6c8405851343f
Jake Marsh <jakemmarsh@gmail.com>
2015-05-13 19:33:50 -0400

    add optional size prop

--- js/components/Spinner.jsx
+++ js/components/Spinner.jsx
@@ -5,15 +5,24 @@ var React = require('react/addons');
 var Spinner = React.createClass({
 
   propTypes: {
-    loading: React.PropTypes.bool.isRequired
+    loading: React.PropTypes.bool.isRequired,
+    size: React.PropTypes.oneOfType([
+      React.PropTypes.string,
+      React.PropTypes.number
+    ])
   },
 
   render: function() {
     var element = null;
+    var styles = {};
+
+    if ( this.props.size ) {
+      styles.fontSize = this.props.size;
+    }
 
     if ( this.props.loading ) {
       element = (
-        <i className="fa fa-spinner fa-spin" />
+        <i className="fa fa-spinner fa-spin" style={styles} />
       );
     }
 
